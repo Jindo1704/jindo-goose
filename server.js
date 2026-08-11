e574770e137a6fcaa60e1565883bca026d47b404
Jindo1704 <dbsehgursl@gmail.com>
2026-08-11 14:37:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,30 +5,24 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
-// 방 데이터 저장소
 const rooms = {};
 
 io.on('connection', (socket) => {
-    // 1. 방 목록 전송 (비밀번호는 숨기고 전송)
     const sendRoomList = () => {
         const roomList = Object.keys(rooms).map(roomName => ({
             roomName, userCount: rooms[roomName].users.length
         }));
         io.emit('room_list', roomList);
     };
-    sendRoomList(); // 처음 접속한 유저에게 방 목록 보여주기
+    sendRoomList();
 
-    // 2. 방 만들기
     socket.on('create_room', (data) => {
-        if (rooms[data.room]) {
-            return socket.emit('room_error', '이미 존재하는 방 이름입니다.');
-        }
+        if (rooms[data.room]) return socket.emit('room_error', '이미 존재하는 방 이름입니다.');
         rooms[data.room] = { password: data.password, users: [], snapshots: [] };
-        sendRoomList(); // 모든 사람의 로비 화면 갱신
+        sendRoomList();
         socket.emit('room_created', data.room);
     });
 
-    // 3. 방 입장 (비밀번호 확인 및 유저 등록)
     socket.on('join_room', (data) => {
         const room = rooms[data.room];
         if (!room) return socket.emit('room_error', '존재하지 않는 방입니다.');
@@ -37,38 +31,48 @@ io.on('connection', (socket) => {
         socket.join(data.room);
         socket.room = data.room;
         socket.nickname = data.nickname;
+        socket.color = data.color; // 유저의 고유 색상 저장
 
-        room.users.push({ id: socket.id, nickname: data.nickname });
+        room.users.push({ id: socket.id, nickname: data.nickname, color: data.color });
         
-        // 방에 있는 사람들에게 접속자 명단, 채팅, 스냅샷 목록 갱신해주기
-        io.to(data.room).emit('update_user_list', room.users.map(u => u.nickname));
-        io.to(data.room).emit('receive_chat', { sender: '📢 시스템', msg: `${data.nickname}님이 입장하셨습니다.` });
+        // 접속자 명단(색상 포함) 갱신
+        io.to(data.room).emit('update_user_list', room.users);
+        io.to(data.room).emit('receive_chat', { sender: '📢 시스템', msg: `${data.nickname}님이 입장하셨습니다.`, color: '#a6adc8' });
         socket.emit('update_snapshots', room.snapshots);
         
         socket.emit('join_success', data.room);
         sendRoomList();
     });
 
-    // 4. 실시간 채팅
     socket.on('send_chat', (data) => {
         if (socket.room) {
-            io.to(socket.room).emit('receive_chat', { sender: socket.nickname, msg: data.msg });
+            io.to(socket.room).emit('receive_chat', { sender: socket.nickname, msg: data.msg, color: socket.color });
         }
     });
 
-    // 5. 미니맵 스냅샷 저장
+    // 마우스 커서 실시간 동기화
+    socket.on('mouse_move', (data) => {
+        if (socket.room) {
+            socket.to(socket.room).emit('update_mouse', {
+                id: socket.id,
+                x: data.x,
+                y: data.y,
+                nickname: socket.nickname,
+                color: socket.color
+            });
+        }
+    });
+
     socket.on('save_snapshot', (data) => {
         if (socket.room && rooms[socket.room]) {
             const room = rooms[socket.room];
             const snapshotName = `Round ${room.snapshots.length + 1} (${new Date().toLocaleTimeString('ko-KR')})`;
             room.snapshots.push({ name: snapshotName, data: data.snapshotData });
-            
             io.to(socket.room).emit('update_snapshots', room.snapshots);
-            io.to(socket.room).emit('receive_chat', { sender: '📸 시스템', msg: `[${snapshotName}] 미니맵이 저장되었습니다.` });
+            io.to(socket.room).emit('receive_chat', { sender: '📸 시스템', msg: `[${snapshotName}] 미니맵이 저장되었습니다.`, color: '#a6e3a1' });
         }
     });
 
-    // 기존 맵 데이터 동기화 기능들
     socket.on('update_input', (data) => socket.to(socket.room).emit('receive_input', data));
     socket.on('move_marker', (data) => socket.to(socket.room).emit('update_marker', data));
     socket.on('add_corpse', (data) => socket.to(socket.room).emit('receive_add_corpse', data));
@@ -76,16 +80,17 @@ io.on('connection', (socket) => {
     socket.on('reset_markers', () => socket.to(socket.room).emit('receive_reset_markers'));
     socket.on('reset_all', () => socket.to(socket.room).emit('receive_reset_all'));
 
-    // 접속 종료 시 (유저 명단에서 삭제)
     socket.on('disconnect', () => {
         if (socket.room && rooms[socket.room]) {
             const room = rooms[socket.room];
             room.users = room.users.filter(u => u.id !== socket.id);
             
-            io.to(socket.room).emit('update_user_list', room.users.map(u => u.nickname));
-            io.to(socket.room).emit('receive_chat', { sender: '📢 시스템', msg: `${socket.nickname}님이 퇴장하셨습니다.` });
+            io.to(socket.room).emit('update_user_list', room.users);
+            io.to(socket.room).emit('receive_chat', { sender: '📢 시스템', msg: `${socket.nickname}님이 퇴장하셨습니다.`, color: '#f38ba8' });
             
-            // 방에 아무도 안 남으면 방 폭파
+            // 퇴장한 사람의 마우스 커서 삭제 명령
+            io.to(socket.room).emit('remove_cursor', socket.id);
+            
             if (room.users.length === 0) delete rooms[socket.room];
             sendRoomList();
         }
