219acaae7e833f020b8759e9e042c1e280bf2e4c
Jindo1704 <dbsehgursl@gmail.com>
2026-08-11 14:41:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.static('public'));
 const rooms = {};
 
 io.on('connection', (socket) => {
+    // 사이트에 접속하면 기본적으로 'lobby' 방에 입장시킵니다.
+    socket.join('lobby');
+
     const sendRoomList = () => {
         const roomList = Object.keys(rooms).map(roomName => ({
             roomName, userCount: rooms[roomName].users.length
@@ -15,6 +18,11 @@ io.on('connection', (socket) => {
         io.emit('room_list', roomList);
     };
     sendRoomList();
+
+    // 로비 전용 채팅
+    socket.on('send_lobby_chat', (data) => {
+        io.to('lobby').emit('receive_lobby_chat', data);
+    });
 
     socket.on('create_room', (data) => {
         if (rooms[data.room]) return socket.emit('room_error', '이미 존재하는 방 이름입니다.');
@@ -28,14 +36,16 @@ io.on('connection', (socket) => {
         if (!room) return socket.emit('room_error', '존재하지 않는 방입니다.');
         if (room.password !== data.password) return socket.emit('room_error', '비밀번호가 틀렸습니다.');
 
+        // 게임 방에 들어가면 로비 채팅방에서는 나갑니다.
+        socket.leave('lobby');
         socket.join(data.room);
+        
         socket.room = data.room;
         socket.nickname = data.nickname;
-        socket.color = data.color; // 유저의 고유 색상 저장
+        socket.color = data.color; 
 
         room.users.push({ id: socket.id, nickname: data.nickname, color: data.color });
         
-        // 접속자 명단(색상 포함) 갱신
         io.to(data.room).emit('update_user_list', room.users);
         io.to(data.room).emit('receive_chat', { sender: '📢 시스템', msg: `${data.nickname}님이 입장하셨습니다.`, color: '#a6adc8' });
         socket.emit('update_snapshots', room.snapshots);
@@ -50,15 +60,10 @@ io.on('connection', (socket) => {
         }
     });
 
-    // 마우스 커서 실시간 동기화
     socket.on('mouse_move', (data) => {
         if (socket.room) {
             socket.to(socket.room).emit('update_mouse', {
-                id: socket.id,
-                x: data.x,
-                y: data.y,
-                nickname: socket.nickname,
-                color: socket.color
+                id: socket.id, x: data.x, y: data.y, nickname: socket.nickname, color: socket.color
             });
         }
     });
@@ -87,8 +92,6 @@ io.on('connection', (socket) => {
             
             io.to(socket.room).emit('update_user_list', room.users);
             io.to(socket.room).emit('receive_chat', { sender: '📢 시스템', msg: `${socket.nickname}님이 퇴장하셨습니다.`, color: '#f38ba8' });
-            
-            // 퇴장한 사람의 마우스 커서 삭제 명령
             io.to(socket.room).emit('remove_cursor', socket.id);
             
             if (room.users.length === 0) delete rooms[socket.room];
